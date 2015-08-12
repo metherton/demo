8c60cf767115a45b3be1e628b45da4d3fadf05fb
metherton <martinetherton63@googlemail.com>
2015-08-12 22:45:47 +0200

    first commit

---
+++ karma.conf.js
@@ -0,0 +1,45 @@
+module.exports = function(config){
+  config.set({
+
+      preprocessors: {
+          // source files, that you wanna generate coverage for
+          // do not include tests or libraries
+          // (these files will be instrumented by Istanbul)
+          'app/**/*.js': ['coverage']
+      },
+
+    basePath : './',
+
+      reporters: ['progress', 'coverage'],
+
+      coverageReporter: { type : 'html', dir : 'coverage/' },
+
+    files : [
+      'app/bower_components/angular/angular.js',
+      'app/bower_components/angular-route/angular-route.js',
+      'app/bower_components/angular-mocks/angular-mocks.js',
+      'app/components/**/*.js',
+      'app/view*/**/*.js'
+    ],
+
+    autoWatch : true,
+
+    frameworks: ['jasmine'],
+
+    browsers : ['Chrome'],
+
+    plugins : [
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-jasmine',
+            'karma-junit-reporter',
+            'karma-coverage'
+            ],
+
+    junitReporter : {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    }
+
+  });
+};
