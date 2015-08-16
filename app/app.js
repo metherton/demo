401e58af8f5e2b5cf062c748bb949c15e12d9995
metherton <martinetherton63@googlemail.com>
2015-08-16 22:56:38 +0200

    fixed as per john papa

--- app/app.js
+++ app/app.js
@@ -1,12 +1,20 @@
-'use strict';
+(function() {
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+  'use strict';
+
+  // Declare app level module which depends on views, and components
+  angular.module('myApp', [
+    'ngRoute',
+    'myApp.view1',
+    'myApp.view2',
+    'myApp.version'
+  ]).
+  config(configure);
+
+  configure.$inject = ['$routeProvider'];
+
+  function configure ($routeProvider) {
+    $routeProvider.otherwise({redirectTo: '/view1'});
+  }
+
+})();
