b27e511f9d5e93425677d2726608b8839becac92
metherton <martinetherton63@googlemail.com>
2015-09-17 23:34:18 +0200

    works but first tab does not actually become active

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -7,8 +7,9 @@ var View1Page = require('./pages/view1.page.js');
 
 describe('my app', function() {
 
+  var handlesLoaded = false;
 
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
+  xit('should automatically redirect to /view1 when location hash/fragment is empty', function() {
     browser.get('index.html');
     expect(browser.getLocationAbsUrl()).toMatch("/view1");
   });
@@ -17,6 +18,7 @@ describe('my app', function() {
   describe('view1', function() {
 
     var page;
+    var handle1, handle2;
 
     beforeEach(function() {
       page = new View1Page();
@@ -73,7 +75,7 @@ describe('my app', function() {
     });
 
 
-    it('should render view1 when user navigates to /view1', function() {
+    xit('should render view1 when user navigates to /view1', function() {
       expect(page.descriptionText.getText()).
         toMatch(/partial for view 1/);
 
@@ -90,14 +92,75 @@ describe('my app', function() {
     });
 
     it('should be possible to switch tabs', function() {
-      page.nuLink.click();
-      browser.sleep(3000);
+
+      var EC = protractor.ExpectedConditions;
+      var titlePresent = EC.titleContains('NU');
+
+      browser.getWindowHandle().then(function(handle) {
+        handle1 = handle;
+        console.log('handle1', handle1);
+      });
+
+      page.nuLink.click().then(function() {
+
+        browser.ignoreSynchronization = true;
+
+//        browser.driver.wait(titlePresent, 20000);
+
+        //browser.driver.wait(function() {
+        //  return browser.driver.getCurrentUrl().then(function(url) {
+        //    console.log('url', url);
+        //    return /nu/.test(url);
+        //  });
+        //}, 20000);
+
+        browser.sleep(15000);
+
+        browser.getAllWindowHandles().then(function(handles) {
+          console.log('handles', handles);
+          //handle1 = handles[0];
+          //handle2 = handles[1];
+          console.log('handle1', handles[0]);
+          console.log('handle2', handles[1]);
+
+          //browser.switchTo().window(handle2);
+          //browser.sleep(5000);
+          browser.ignoreSynchronization = true;
+          browser.switchTo().window(handles[1]).then(function() {
+            browser.sleep(1000);
+          });
+
+          browser.getCurrentUrl().then(function(url) {
+            console.log('url is ', url);
+          });
+
+//          browser.ignoreSynchronization = false;
+         // browser.driver.close();
+          browser.switchTo().window(handle1).then(function() {
+            browser.sleep(1000);
+          });
+
+
+
+          browser.ignoreSynchronization = false;
+
+          browser.getCurrentUrl().then(function(url) {
+            console.log('original url is ', url);
+          });
+
+          browser.sleep(1000);
+
+        })
+      });
+
+      browser.sleep(1000);
+
     });
 
   });
 
 
-  describe('view2', function() {
+  xdescribe('view2', function() {
 
     beforeEach(function() {
       browser.get('index.html#/view2');
